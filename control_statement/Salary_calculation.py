4668a6be4087ec91f508784048d93cd301b907b4
Sneha-Khatri <snehakhatri1603@gmail.com>
2026-02-19 11:09:27 +0530

    first commit

---
+++ control_statement/Salary_calculation.py
@@ -0,0 +1,10 @@
+# Salary Calculation
+
+basic_salary = 25000
+hra = (20 * basic_salary) / 100
+bonus = 5000
+
+total_salary = basic_salary + hra + bonus
+
+print("HRA =", hra)
+print("Total Monthly Salary =", total_salary)
---
+++ control_statement/perimeter.py
@@ -0,0 +1,8 @@
+radius = float(input("Enter radius: "))
+pi = 3.14
+
+area = pi * radius * radius
+perimeter = 2 * pi * radius
+
+print("Area =", area)
+print("Perimeter =", perimeter)
