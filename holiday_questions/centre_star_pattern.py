00465e550ed0377eca58fbc748d4a81df46c7ccd
Sneha-Khatri <snehakhatri1603@gmail.com>
2026-03-13 22:58:37 +0530

    Added Center star pattern

---
+++ holiday_questions/centre_star_pattern.py
@@ -0,0 +1,15 @@
+n = 5
+
+for i in range(n):
+    for j in range(n):
+        if j == n//2:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+#Output
+"""*     
+    *
+    *
+    *
+    *"""
